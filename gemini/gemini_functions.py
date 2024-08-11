47359f7a4a7861a38d006c2401e071370739f0ee
sinking8 <ashwinblaze111@gmail.com>
2024-08-11 12:53:50 -0700

    fixed projections

--- gemini/gemini_functions.py
+++ gemini/gemini_functions.py
@@ -68,7 +68,6 @@ prompt_autocorrect = collection_gemini.find({"model": "autocorrect"})[0]["prompt
 prompt_stockOpinion = collection_gemini.find({"model": "stockOpinion"})[0]["prompt"]
 prompt_projection = collection_gemini.find({"model": "projection"})[0]["prompt"]
 prompt_recommendation = collection_gemini.find({"model": "recommendation"})[0]["prompt"]
-prompt_expandWatchlist = collection_gemini.find({"model": "expandWatchlist"})[0]["prompt"]
 
 # Close the MongoDB connection
 client.close()
@@ -86,7 +85,6 @@ def get_recommendations(profile: str):
             genai.configure(api_key=API_KEY)
             model = genai.GenerativeModel(
                 "gemini-1.5-flash",
-                system_instruction="",
                 generation_config={"response_mime_type": "application/json"},
             )
             response = model.generate_content(prompt_recommendation)
@@ -98,11 +96,10 @@ def get_recommendations(profile: str):
             if num_hits <= 0:
                 return {"text": "ERROR"}
 
-
 @app.get("/get_projections")
 def get_projections(object_string: str):
     global prompt_projection
-    values = eval(object_string["results"])
+    values = json.loads(object_string)["results"]
     values = list(map(lambda x: str(x["v"]), values))
     values = "[" + ",".join(values) + "]"
 
@@ -114,7 +111,6 @@ def get_projections(object_string: str):
             genai.configure(api_key=API_KEY)
             model = genai.GenerativeModel(
                 "gemini-1.5-flash",
-                system_instruction="",
                 generation_config={"response_mime_type": "application/json"},
             )
             response = model.generate_content(prompt_projection)
@@ -194,37 +190,8 @@ def stockOpinion(ticker: str):
             if num_hits <= 0:
                 return {"text": "ERROR"}
 
-@app.get('/expandwatchlist')
-def expandWatchlist():
-    response = requests.get("http://localhost:5000/get_watchlist") 
-    response.raise_for_status()  # Ensure that the request was successful
-    watchlist = response.json()  # Use the .json() method to parse the response JSON
 
-    symbols = [company['symbol'] for company in watchlist.get('selected_companies', [])]
-
-    num_hits = len(GEMINI_API_KEYS)
-    while GEMINI_API_KEYS:
-        API_KEY = secrets.choice(GEMINI_API_KEYS)
-        try:
-            genai.configure(api_key=API_KEY)
-            model = genai.GenerativeModel(
-                "gemini-1.5-flash",
-                system_instruction=prompt_expandWatchlist,
-                generation_config={"response_mime_type": "application/json"},
-            )
-            response = model.generate_content(symbols)
-            num_hits += 1
-
-            return json.loads(response.text)
-
-        except InvalidArgument as e:
-            logging.warning(f"Model cannot generate with API key {API_KEY}: {e}")
-            num_hits -= 1
-            if num_hits <= 0:
-                return {"text": "ERROR"}
-            
-
-# News API Classgem
+# News API Class
 class NewsAPI:
     def __init__(self, api_key=None):
         try:
@@ -311,4 +278,4 @@ class Summarizer:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="localhost", port=8080)
